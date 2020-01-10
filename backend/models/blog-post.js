06ee400b049bb55b4fc332da4f6d759e16845c8a
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-10 17:39:47 +0100

    Add blog post schema

--- backend/models/blog-post.js
+++ backend/models/blog-post.js
@@ -2,5 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var blogPostSchema = new Schema({
-    title: String
+    title: String,
+    user: String,
+    timestamp: Date,
+    comments: [{
+        user: String,
+        comment: String,
+        timestamp: Date
+    }]
 });
+
+module.exports = mongoose.model('users', userSchema);
