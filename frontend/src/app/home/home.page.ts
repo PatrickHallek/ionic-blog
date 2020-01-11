bc510fa8616f02a8d8bc1e282df373907bbef15b
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-11 18:17:37 +0100

    Use fetched data in home component

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { BlogPostViewComponent } from '../components/blog-post-view/blog-post-view.component';
+import { Subscription } from 'rxjs';
+import { BlogPostService } from '../service/blog-post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
+  private blogPostsListenerSub: Subscription;
+  public blogPosts;
 
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController, private blogPostService: BlogPostService) { }
+
+  ngOnInit() {
+    this.blogPostService.getBlogPosts();
+    this.blogPostsListenerSub = this.blogPostService
+      .getBlogPostListener().subscribe(blogPosts => this.blogPosts = blogPosts);
+  }
 
   async openBlogPost(blogPost) {
     const modal = await this.modalController.create({
@@ -18,6 +28,10 @@ export class HomePage {
       componentProps: blogPost
     });
     return await modal.present();
+  }
+
+  ngOnDestroy() {
+    this.blogPostsListenerSub.unsubscribe();
   }
 
 }
