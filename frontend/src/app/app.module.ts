8d153b39d642d163ffd670f12f2420270a7499d3
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-11 19:12:21 +0100

    Add http client module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -15,7 +16,13 @@ import { BlogPostViewComponent } from './components/blog-post-view/blog-post-vie
 @NgModule({
   declarations: [AppComponent, BlogPostViewComponent],
   entryComponents: [BlogPostViewComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
