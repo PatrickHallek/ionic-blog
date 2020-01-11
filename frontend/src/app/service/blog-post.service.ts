589fa2606dddb9ba10e1018ae573cda1601b114a
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-11 18:17:20 +0100

    Add api service

--- frontend/src/app/service/blog-post.service.ts
+++ frontend/src/app/service/blog-post.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostService {
+  private blogPosts = new Subject<any>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  public getBlogPostListener() {
+    return this.blogPosts.asObservable();
+  }
+
+  public getBlogPosts() {
+    this.http.get('http://localhost:3000/blog-posts')
+      .subscribe((blogPosts) => {
+        this.blogPosts.next(blogPosts);
+      }, err => console.error(err));
+  }
+
+  public createBlogPost(user, content) {
+    const send = { user, content };
+    this.http.post('http://localhost:3000/blog-post', send)
+      .subscribe(() => { }, err => console.error(err));
+  }
+
+  public createBlogPostComment() {
+    this.http.put('http://localhost:3000/blog-post-comment', {})
+      .subscribe(() => { }, err => console.error(err)
+      );
+  }
+
+
 }
