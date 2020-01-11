1330da5192e1c0b7b500de860d113b92c062c5d5
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-11 18:00:51 +0100

    Add blog post comment api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,6 +11,7 @@ router.get('/blog-posts', function (req, res) {
 });
 
 router.post('/blog-post', function (req, res, next) {
+
     var blogPost = new BlogPost({
         title: req.body.title,
         content: req.body.content,
@@ -18,9 +19,40 @@ router.post('/blog-post', function (req, res, next) {
         timestamp: Date.now(),
         comments: []
     })
+
     blogPost.save().then(data => {
             res.status(200).json({
                 message: "New blog post created",
+                data: data
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "Something went wrong"
+            });
+        });
+});
+
+router.put('/blog-post-comment', function (req, res, next) {
+
+    console.log(req.body)
+    var conditions = {
+        _id: req.body.id
+    }
+    var update = {
+        $push: {
+            comments: {
+                user: req.body.user,
+                content: req.body.content,
+                timestamp: Date.now()
+            }
+        }
+    }
+
+    BlogPost.update(conditions, update).then(data => {
+            res.status(200).json({
+                message: "New comment created",
                 data: data
             });
         })
