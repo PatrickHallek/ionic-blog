0f01a510e9faa7d8b6c5a9bd528266b7cecf6a03
PatrickHallek <patrickhallek1998@gmail.com>
2020-01-12 16:00:30 +0100

    Add types to blog post service functions

--- frontend/src/app/service/blog-post.service.ts
+++ frontend/src/app/service/blog-post.service.ts
@@ -23,12 +23,12 @@ export class BlogPostService {
       }, err => console.error(err));
   }
 
-  public createBlogPost(user, title, content) {
+  public createBlogPost(user: string, title: string, content: string) {
     this.http.post('http://localhost:3000/blog-post', { user, title, content })
       .subscribe(() => { this.getBlogPosts(); }, err => console.error(err));
   }
 
-  public createBlogPostComment(id, user, content) {
+  public createBlogPostComment(id: string, user: string, content: string) {
     this.http.put('http://localhost:3000/blog-post-comment', { id, user, content })
       .subscribe(() => { this.getBlogPosts(); }, err => console.error(err)
       );
